1c564b748eec7399c63aafdf5c6f1872320a037a
charlesgael <charlesgael.djedai@outlook.com>
2025-10-23 16:56:48 +0200

    Dont do ssg build for gh pages

--- src/main.ts
+++ src/main.ts
@@ -11,25 +11,13 @@ import '@unocss/reset/tailwind.css'
 import 'uno.css'
 import './styles/main.css'
 
-// export function createWebHashHistory(base?: string): RouterHistory {
-//   // Make sure this implementation is fine in terms of encoding, specially for IE11
-//   // for `file://`, directly use the pathname and ignore the base
-//   // location.pathname contains an initial `/` even at the root: `https://example.com`
-//   base = location && location.host ? base || location.pathname + location.search : ''
-//   // allow the user to provide a `#` in the middle: `/base/#/app`
-//   if (!base.includes('#'))
-//     base += '#'
-
-//   return createWebHistory(base)
-// }
-
 // https://github.com/antfu/vite-ssg
 export const createApp = ViteSSG(
   App,
   {
     routes: setupLayouts(routes),
     base: import.meta.env.BASE_URL,
-    history: import.meta.env.MODE === 'dev' ? createWebHistory() : createWebHashHistory(),
+    history: import.meta.env.MODE === 'development' ? createWebHistory() : createWebHashHistory(),
   },
   async (ctx) => {
     // install all modules under `modules/`
